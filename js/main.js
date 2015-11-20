8acc61dde425c3ff3c152793f750b5a8c1549fee
Marcela Salazar <Marcela@marcela-salazars-macbook.local>
2015-11-20 12:27:59 -0500

    font folder added

--- js/main.js
+++ js/main.js
@@ -2,18 +2,20 @@
 $(document).keydown(function(e) {
 
     var position = $('#charlie').position();
+
                 
     switch(e.keyCode) {
-        case 37: //left
-        $('#charlie').css('left', position.left - 20 + 'px');
-        break;
 
-        case 39:
+        case 39: // right
         $('#charlie').css('left', position.left + 20 + 'px');
         break;
-                       
+
+        case 37: // left
+        $('#charlie').css('left', position.left - 20 + 'px');
     }
 
     return false;
 });
+
+
 
