be6ca9dddfbc4a2ce63856ab1dd1c4e433597a64
Graphic <graphic@SOD-SJH-516-23M.local>
2015-11-20 14:38:03 -0500

    jquery flip img

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
+var conW = $(window).width() - $('.container').width();
+// console.log (conW)
 
 $(document).keydown(function(e) {
 
     var position = $('#charlie').position();
-
                 
     switch(e.keyCode) {
-
+        
         case 39: // right
-        $('#charlie').css('left', position.left + 20 + 'px');
+        $('#charlie').css('left', position.left + 20 + 'px').removeClass('flip');
         break;
 
         case 37: // left
-        $('#charlie').css('left', position.left - 20 + 'px');
+        $('#charlie').css('left', position.left - 20 + 'px').addClass('flip');
+                         
     }
 
-    return false;
+
 });
-
-
 
